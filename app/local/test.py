1177f03ce23108d25fc962fcfa5f12fda5ebc9d4
Nicholas Poon <poontzejian@gmail.com>
2025-09-12 17:13:33 +0800

    Modifying test.py to add more images for testing calling the endpoint

--- app/local/test.py
+++ app/local/test.py
@@ -1,9 +1,11 @@
 import requests
 
 files = [
-    ('files', open('app/local/Images/image1.png', 'rb')),
-    ('files', open('app/local/Images/image2.png', 'rb')),
-    ('files', open('app/local/Images/image3.jpeg', 'rb'))
+    ('files', open('app/local/Images/image1.jpg', 'rb')),
+    ('files', open('app/local/Images/image2.jpeg', 'rb')),
+    ('files', open('app/local/Images/image3.jpeg', 'rb')),
+    ('files', open('app/local/Images/image4.jpg', 'rb')),
+    ('files', open('app/local/Images/image5.jpeg', 'rb'))
 ]
 response = requests.post("http://localhost:8000/api/v1/fire-detection/detect_fire/", files=files)
 print(response.json())
