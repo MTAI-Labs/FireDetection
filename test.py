a6ca36c471b47e13d26026b129c9a74d1a6aa01f
Nicholas Poon <poontzejian@gmail.com>
2025-09-12 14:15:13 +0800

    initial commit

---
+++ test.py
@@ -0,0 +1,8 @@
+import requests
+
+files = [
+    ('files', open('app/local/Images/image1.png', 'rb')),
+    ('files', open('app/local/Images/image2.png', 'rb'))
+]
+response = requests.post("http://localhost:8000/api/v1/fire-detection/detect_fire/", files=files)
+print(response.json())
